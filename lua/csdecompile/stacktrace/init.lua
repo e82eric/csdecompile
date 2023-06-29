511543f6a4113706741635c8ccc0a83154387947
Eric Nelson <e82.eric@gmail.com>
2023-06-29 00:17:24 -0700

    Added user command for parsing stack trace

--- lua/csdecompile/stacktrace/init.lua
+++ lua/csdecompile/stacktrace/init.lua
@@ -7,38 +7,38 @@ M._tbl_length = function(T)
 end
 
 M._get_visual_selection = function()
-    -- this will exit visual mode
-    -- use 'gv' to reselect the text
-    local _, csrow, cscol, cerow, cecol
-    local mode = vim.fn.mode()
-    if mode == 'v' or mode == 'V' or mode == '' then
-      -- if we are in visual mode use the live position
-      _, csrow, cscol, _ = unpack(vim.fn.getpos("."))
-      _, cerow, cecol, _ = unpack(vim.fn.getpos("v"))
-      if mode == 'V' then
-        -- visual line doesn't provide columns
-        cscol, cecol = 0, 999
-      end
-      -- exit visual mode
-      vim.api.nvim_feedkeys(
-        vim.api.nvim_replace_termcodes("<Esc>",
-          true, false, true), 'n', true)
-    else
-      -- otherwise, use the last known visual position
-      _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
-      _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  -- this will exit visual mode
+  -- use 'gv' to reselect the text
+  local _, csrow, cscol, cerow, cecol
+  local mode = vim.fn.mode()
+  if mode == 'v' or mode == 'V' or mode == '' then
+    -- if we are in visual mode use the live position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos("."))
+    _, cerow, cecol, _ = unpack(vim.fn.getpos("v"))
+    if mode == 'V' then
+      -- visual line doesn't provide columns
+      cscol, cecol = 0, 999
     end
-    -- swap vars if needed
-    if cerow < csrow then csrow, cerow = cerow, csrow end
-    if cecol < cscol then cscol, cecol = cecol, cscol end
-    local lines = vim.fn.getline(csrow, cerow)
-    -- local n = cerow-csrow+1
-    local n = M._tbl_length(lines)
-    if n <= 0 then return '' end
-    lines[n] = string.sub(lines[n], 1, cecol)
-    lines[1] = string.sub(lines[1], cscol)
-    return lines
-    -- return table.concat(lines, "\n")
+    -- exit visual mode
+    vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes("<Esc>",
+    true, false, true), 'n', true)
+  else
+    -- otherwise, use the last known visual position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
+    _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  end
+  -- swap vars if needed
+  if cerow < csrow then csrow, cerow = cerow, csrow end
+  if cecol < cscol then cscol, cecol = cecol, cscol end
+  local lines = vim.fn.getline(csrow, cerow)
+  -- local n = cerow-csrow+1
+  local n = M._tbl_length(lines)
+  if n <= 0 then return '' end
+  lines[n] = string.sub(lines[n], 1, cecol)
+  lines[1] = string.sub(lines[1], cscol)
+  return lines
+  -- return table.concat(lines, "\n")
 end
 
 M._parse_stack_frames = function(sourceLines)
@@ -88,6 +88,16 @@ end
 M.run = function()
   local lines = M._get_visual_selection()
   M._set_buffer(lines)
+end
+
+M.setup = function()
+  vim.api.nvim_create_user_command(
+  'ParseStackTrace',
+  function(opts)
+    M.run()
+  end,
+  {range = true}
+  )
 end
 
 return M
